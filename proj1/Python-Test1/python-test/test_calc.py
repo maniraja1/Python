982a220ee55e22d2c61c6d2232b148641eb0abf0
Manishankar Rajagopal <mrajagopal@mrajagopal-ltm.internal.salesforce.com>
2018-07-05 17:10:27 -0700

    more changes

---
+++ proj1/Python-Test1/python-test/test_calc.py
@@ -0,0 +1,36 @@
+import calc
+import unittest
+
+'''
+ one way of running this is navigate to the project folder where your test.py is located and run the below
+ python -m unittest test_calc.py
+
+ Another way of running thsi is including if__name=='__main__':unittest.main()
+
+'''
+class testcalc(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calc.add(10,5),15)
+        self.assertEqual(calc.add(1, -1), 0)
+        self.assertEqual(calc.add(-5, -5), -10)
+
+    def test_subtract(self):
+        self.assertEqual(calc.subtract(10,5),5)
+        self.assertEqual(calc.subtract(1, -1), 2)
+        self.assertEqual(calc.subtract(-1, -5), 4)
+
+    def test_multiply(self):
+        self.assertEqual(calc.multiply(10,5),50)
+        self.assertEqual(calc.multiply(1, -1), -1)
+        self.assertEqual(calc.multiply(-1, -5), 5)
+
+    def test_divide(self):
+        self.assertEqual(calc.divide(10,5),2)
+        self.assertEqual(calc.divide(1, -1), -1)
+        self.assertEqual(calc.divide(-1, -5), 0.2)
+
+
+
+if __name__=='__main__':
+    unittest.main()
---
+++ proj1/Python-Test1/python-test/test_employee.py
@@ -0,0 +1,67 @@
+from employee import employee
+import unittest
+from unittest.mock import patch
+
+class Testemployee(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass within superclass")
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass within SuperClass")
+
+    def setUp(self):
+        self.emp1=employee('corey','schafer',50000)
+        self.emp2=employee('mani','raja',60000)
+
+    def tearDown(self):
+        print('tearDown\n')
+
+    def test_email(self):
+        self.assertEqual(self.emp1.email,'corey.schafer@email.com')
+        self.assertEqual(self.emp2.email, 'mani.raja@email.com')
+
+        self.emp1.first='john'
+        self.emp2.first='jane'
+
+        self.assertEqual(self.emp1.email, 'john.schafer@email.com')
+        self.assertEqual(self.emp2.email, 'jane.raja@email.com')
+
+    def test_fullname(self):
+        self.assertEqual(self.emp1.fullname,'corey schafer')
+        self.assertEqual(self.emp2.fullname,'mani raja')
+
+        self.emp1.first = 'john'
+        self.emp2.first = 'jane'
+
+        self.assertEqual(self.emp1.fullname, 'john schafer')
+        self.assertEqual(self.emp2.fullname, 'jane raja')
+
+    def test_apply_raise(self):
+        self.emp1.apply_raise()
+        self.emp2.apply_raise()
+
+        self.assertEqual(self.emp1.pay, 52500)
+        self.assertEqual(self.emp2.pay, 63000)
+
+    def test_monthly_schedule(self):
+        with patch ('employee.requests.get') as mocked_get:
+            mocked_get.return_value.ok=True
+            mocked_get.return_value.text='Success'
+
+            schedule = self.emp1.monthly_schedule('May')
+            mocked_get.assert_called_with('http://company.com/schafer/May')
+            self.assertEqual(schedule, 'Success')
+
+            mocked_get.return_value.ok = False
+
+            schedule = self.emp2.monthly_schedule('June')
+            mocked_get.assert_called_with('http://company.com/raja/June')
+            self.assertEqual(schedule, 'Bad Response!')
+
+
+
+
+if __name__=='__main__':
+    unittest.main()
