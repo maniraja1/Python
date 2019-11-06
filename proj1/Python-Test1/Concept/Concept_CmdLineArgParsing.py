9c5bad8f92ca802bc543967ce0d061f560b6e933
Manishankar Rajagopal <mrajagopal@mrajagopal-ltm.internal.salesforce.com>
2019-11-06 10:27:30 -0800

    Moved concepts to folder

--- proj1/Python-Test1/Concept/Concept_CmdLineArgParsing.py
+++ proj1/Python-Test1/Concept/Concept_CmdLineArgParsing.py
@@ -1,15 +1,24 @@
 '''
-    How to parse command line arguments in python
+Quick Concept
+    How to parse command line arguments in python. You have the following options
     1.sysargv
     2.getopt
     3.argparse
+Module
+    import argparse
+Core Methods
+    ArgumentParser()   -   Creates an object of type argument parser
+    add_argument()     -   Add arguments to object of type Argument parser
+    add_mutually_exclusive_group()    - Add mutually exclusive argument
+    parse_args()       - Parse argument from object of type Argument parser
 '''
 '''
-    Example how to use argoarse.
+Example how to use argparse.
     Save the below code and run the below command to execute from command line
         python test3.py --help
        python test3.py --a 1 --b 2 --filename "test.py" --education "highschool" --d --e --f 
     Above command cannot use positional argument, Every argument needs to be passed with argument name
+Options
     Choices
         You can pass a list of valid choice
     Action
@@ -51,9 +60,10 @@ print(f"F: {f}")
 print(f"G: {g}")
 
 '''
+Example sys.argv
     Parsing agrument using sys.argv
     sys.argv is a list with first element being the filename followed by the parameters. 
-    You can only pass positional argument. Keyword arguments are not allowed
+    You can only pass positional argument. Keyword arguments are not allowed.
 '''
 import sys
 print(sys.argv)
